62f8eaa7de5668d1c79413d66d258a4cdfc93932
Seun Adetunji <oluwaseunus@gmail.com>
2019-01-28 19:46:31 +0100

    Updates

---
+++ precache-manifest.7250078114d0a15b5477b8a4ede26b1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b94d10f8de67808168a7",
+    "url": "/Oluwaseunus/Random-Quote-Generator/static/js/runtime~main.b94d10f8.js"
+  },
+  {
+    "revision": "5398254da0cf3fbe15f3",
+    "url": "/Oluwaseunus/Random-Quote-Generator/static/js/main.5398254d.chunk.js"
+  },
+  {
+    "revision": "eee31ed4da0adb96db57",
+    "url": "/Oluwaseunus/Random-Quote-Generator/static/js/1.eee31ed4.chunk.js"
+  },
+  {
+    "revision": "5398254da0cf3fbe15f3",
+    "url": "/Oluwaseunus/Random-Quote-Generator/static/css/main.0ef384ca.chunk.css"
+  },
+  {
+    "revision": "395e3a24fcf876121c5a7c2959cd3376",
+    "url": "/Oluwaseunus/Random-Quote-Generator/index.html"
+  }
+];
